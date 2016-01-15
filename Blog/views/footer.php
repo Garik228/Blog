19e58284b6f9451147de5e4354f453588edeaef3
Garik228 <wall.228@mail.ru>
2016-01-15 22:05:25 +0930

    Добавлен CMS 15.01.2016

---
+++ Blog/views/footer.php
@@ -0,0 +1,19 @@
+<!-- Begin Status Module -->
+<div id="status" class="navbar navbar-fixed-bottom hidden-phone">
+    <div class="btn-toolbar">
+        <div class="btn-group pull-right">
+            <p>
+                Версия 1.0	&nbsp;—&nbsp;
+                © 2016 СТОТиС				</p>
+
+        </div>
+        <div class="btn-group pull-right">
+        </div>
+        <div class="btn-group viewsite">
+            <a href="../index.php" target="_blank"><span class="icon-out-2"></span> Просмотр сайта</a>
+        </div>
+    </div>
+    </div>
+    <!-- End Status Module -->
+    </body>
+</html>
